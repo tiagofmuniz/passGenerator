ff72bd9250169e7772b6b8ab9cd1ec4980dc0f8d
tiagofmuniz <tiagofmuniz2023@gmail.com>
2023-11-17 14:53:54 -0300

    Gerador de senha

---
+++ README.md
@@ -0,0 +1,24 @@
+# Gerador de Senha
+
+## Descrição
+Um simples gerador de senhas que permite personalizar o tamanho da senha e copiá-la para a área de transferência.
+
+![Gerador de Senha Logo](./assets/padlock.png)
+
+## Tecnologias
+- HTML
+- CSS
+- JavaScript
+
+## Como usar
+1. Abra o arquivo `index.html` em seu navegador.
+2. Mova o controle deslizante para escolher o tamanho da senha desejado.
+3. Clique no botão "Gerar senha" para gerar uma nova senha.
+4. Clique na senha gerada para copiá-la para a área de transferência.
+5. Uma mensagem indicando que a senha foi copiada com sucesso será exibida temporariamente.
+
+Certifique-se de incluir o arquivo `index.css` e `index.js` no mesmo diretório que o seu arquivo HTML para garantir o funcionamento adequado do aplicativo.
+
+---
+
+**Nota:** Certifique-se de substituir `./assets/padlock.png` pelo caminho correto para a imagem do seu logo, se estiver em um diretório diferente.
---
+++ index.js
@@ -0,0 +1,41 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function () {
+  sizePassword.innerHTML = this.value;
+};
+
+function generatePassword() {
+  let pass = "";
+  for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  console.log(pass);
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copyPassword() {
+  let messageDiv = document.createElement("div");
+  messageDiv.classList.add("copy-message");
+  messageDiv.innerHTML = "Senha copiada com sucesso!";
+
+  containerPassword.appendChild(messageDiv);
+
+  navigator.clipboard.writeText(novaSenha);
+  setTimeout(function () {
+    messageDiv.remove();
+  }, 3000);
+}
